b23defd89c1084d90cf4c9d298d493c1de38bb11
shashankparchure <parchure_s@rediffmail.com>
2025-05-07 17:31:29 +0530

    draft2-deployment

--- my-app/app/components/Contact.jsx
+++ my-app/app/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "063a110a-6a86-4d3d-a642-8833ed1a1740");
+    formData.append("access_key", process.env.NEXT_PUBLIC_WEB3FORMS_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
