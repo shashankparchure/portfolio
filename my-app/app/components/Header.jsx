ce93d4c20344c25a7c2aaffd805d4dad69fcf383
shashankparchure <parchure_s@rediffmail.com>
2025-05-20 17:07:30 +0530

    updated resume

--- my-app/app/components/Header.jsx
+++ my-app/app/components/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y:0, opacity: 1}}
             transition={{duration: 0.6, delay:1.2}} 
-            href="/sample-resume.pdf" download 
+            href="https://drive.google.com/file/d/1DgRXkTXhLU9smEI7TWFG5bAxO7fYaD1I/view?usp=sharing" download 
             className='px-10 py-3 border rounded-full border-gray-500 flex
             items-center gap-2'> 
             My Resume <Image src={assets.download_icon} alt='' 
